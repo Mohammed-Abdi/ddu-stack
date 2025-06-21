75687d8173ee05dca2f63094a554a7ec541f490f
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-21 17:16:54 +0300

    style: update Hero component text for improved clarity and emphasis

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,12 @@
+import EmphasizedText from "../emphasized-text/EmphasizedText.jsx";
 import "./Hero.css";
 
 function Hero() {
   return (
     <section className="hero">
       <h1>
-        All Your Course Materials in <br /> One Smart Stack.
+        All Your Course Materials in <br /> One Smart{" "}
+        <span style={{ color: "var(--accent-clr)" }}>Stack</span>
       </h1>
       <p>
         No more digging through folders or broken links. Find exactly what you
