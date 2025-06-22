b935ebdc80dca657efec3e758e62478bca1ddefb
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-22 05:53:51 +0300

    feat: add courses data

---
+++ src/data/courses.js
@@ -0,0 +1,44 @@
+const courses = [
+  {
+    name: "Data Structure And Algorithms",
+    code: "SOEng2022",
+    image: "lorem",
+  },
+  {
+    name: "Digital Logic Design",
+    code: "ECEG2142",
+    image: "lorem",
+  },
+  {
+    name: "Operating Systems",
+    code: "SOEng2032",
+    image: "lorem",
+  },
+  {
+    name: "Object Oriented System Analysis and Design",
+    code: "SOEng2052",
+    image: "lorem",
+  },
+  {
+    name: "Advanced Software Engineering",
+    code: "SOEng2054",
+    image: "lorem",
+  },
+  {
+    name: "Fundamentals of Database Systems",
+    code: "SOEng2042",
+    image: "lorem",
+  },
+  {
+    name: "Fundamentals of Software Engineering",
+    code: "SOEng2051",
+    image: "lorem",
+  },
+  {
+    name: "Fundamentals of Programming II",
+    code: "SOEng2021",
+    image: "lorem",
+  },
+];
+
+export default courses;
