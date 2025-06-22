55e67f6fb7d6c3da6b66c37fcac3c9e60ca16368
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-22 09:04:03 +0300

    feat: enhance Courses component to support search functionality

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -2,30 +2,40 @@ import "./Courses.css";
 import courses from "../../data/courses.js";
 import materials from "../../data/materials.js";
 
-function Courses({ year }) {
+function Courses({ search, year }) {
   return (
     <div className="courses-wrapper">
-      {courses
-        .filter((course) => course.year == year)
-        .map((course, i) => {
-          return (
-            <>
-              <div key={i} className="course">
-                {course.name}
-              </div>
+      {search
+        ? materials
+            .filter((material) =>
+              material.search.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((filteredMaterial) => {
+              return (
+                <div className="material-filtered">{filteredMaterial.name}</div>
+              );
+            })
+        : courses
+            .filter((course) => course.year == year)
+            .map((course, i) => {
+              return (
+                <>
+                  <div key={i} className="course">
+                    {course.name}
+                  </div>
 
-              {materials
-                .filter((material) => material.code === course.code)
-                .map((material, index) => {
-                  return (
-                    <div key={index} className="course-chapters">
-                      {material.name + " " + material.chapter}
-                    </div>
-                  );
-                })}
-            </>
-          );
-        })}
+                  {materials
+                    .filter((material) => material.code === course.code)
+                    .map((material, index) => {
+                      return (
+                        <div key={index} className="course-chapters">
+                          {material.name + " " + material.chapter}
+                        </div>
+                      );
+                    })}
+                </>
+              );
+            })}
     </div>
   );
 }
