1a932342235cf72526070980581dde4de0f79bac
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-22 05:55:07 +0300

    fix: add missing year property to all course entries

--- src/data/courses.js
+++ src/data/courses.js
@@ -3,41 +3,49 @@ const courses = [
     name: "Data Structure And Algorithms",
     code: "SOEng2022",
     image: "lorem",
+    year: 2017,
   },
   {
     name: "Digital Logic Design",
     code: "ECEG2142",
     image: "lorem",
+    year: 2017,
   },
   {
     name: "Operating Systems",
     code: "SOEng2032",
     image: "lorem",
+    year: 2017,
   },
   {
     name: "Object Oriented System Analysis and Design",
     code: "SOEng2052",
     image: "lorem",
+    year: 2017,
   },
   {
     name: "Advanced Software Engineering",
     code: "SOEng2054",
     image: "lorem",
+    year: 2017,
   },
   {
     name: "Fundamentals of Database Systems",
     code: "SOEng2042",
     image: "lorem",
+    year: 2017,
   },
   {
     name: "Fundamentals of Software Engineering",
     code: "SOEng2051",
     image: "lorem",
+    year: 2017,
   },
   {
     name: "Fundamentals of Programming II",
     code: "SOEng2021",
     image: "lorem",
+    year: 2017,
   },
 ];
 
