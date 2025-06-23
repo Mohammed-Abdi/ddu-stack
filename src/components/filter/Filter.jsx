5e425dfa3cd9222d51050beaf5ab265f8bc2f11e
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-23 12:15:27 +0300

    refactor: simplify button click handling in Filter component

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import "./Filter.css";
 
 function Filter({ setYear }) {
+  const [isOnIt, setIsOnIt] = useState(2017);
+  function handleFilter(e) {
+    const value = Number(e.target.value);
+    setYear(value);
+    setIsOnIt(value);
+  }
+
   return (
     <div className="filter">
-      <button value={2017} onClick={(e) => setYear(Number(e.target.value))}>
+      <button
+        value={2017}
+        onClick={handleFilter}
+        className={isOnIt === 2017 ? "highlight" : ""}
+      >
         2nd year
       </button>
-      <button value={2018} onClick={(e) => setYear(Number(e.target.value))}>
+      <button
+        value={2018}
+        onClick={handleFilter}
+        className={isOnIt === 2018 ? "highlight" : ""}
+      >
         3rd year
       </button>
     </div>
