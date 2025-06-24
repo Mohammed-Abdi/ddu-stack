181d8e7868c87b357ed8faa659ca0e03f4776d8f
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-24 14:50:27 +0300

    feat: add 3rd year 1st semester courses

--- src/data/courses.js
+++ src/data/courses.js
@@ -1,51 +1,82 @@
 const courses = [
-  {
-    name: "Data Structure And Algorithms",
-    code: "SOEng2022",
-    image: "lorem",
-    year: 2017,
-  },
-  {
-    name: "Digital Logic Design",
-    code: "ECEG2142",
-    image: "lorem",
-    year: 2017,
-  },
-  {
-    name: "Operating Systems",
-    code: "SOEng2032",
-    image: "lorem",
-    year: 2017,
-  },
-  {
-    name: "Object Oriented System Analysis and Design",
-    code: "SOEng2052",
-    image: "lorem",
-    year: 2017,
-  },
-  {
-    name: "Advanced Software Engineering",
-    code: "SOEng2054",
-    image: "lorem",
-    year: 2017,
-  },
-  {
-    name: "Fundamentals of Database Systems",
-    code: "SOEng2042",
-    image: "lorem",
-    year: 2017,
-  },
+  //2nd year 2nd semester
   {
     name: "Fundamentals of Software Engineering",
     code: "SOEng2051",
-    image: "lorem",
     year: 2017,
   },
   {
     name: "Fundamentals of Programming II",
     code: "SOEng2021",
-    image: "lorem",
     year: 2017,
+  },
+  //2nd year 2nd semester
+  {
+    name: "Data Structure And Algorithms",
+    code: "SOEng2022",
+    year: 2017,
+  },
+  {
+    name: "Digital Logic Design",
+    code: "ECEG2142",
+    year: 2017,
+  },
+  {
+    name: "Operating Systems",
+    code: "SOEng2032",
+    year: 2017,
+  },
+  {
+    name: "Object Oriented System Analysis and Design",
+    code: "SOEng2052",
+    year: 2017,
+  },
+  {
+    name: "Advanced Software Engineering",
+    code: "SOEng2054",
+    year: 2017,
+  },
+  {
+    name: "Fundamentals of Database Systems",
+    code: "SOEng2042",
+    year: 2017,
+  },
+
+  //3rd year 1st semester
+  {
+    name: "Object Oriented Programming",
+    code: "SOEng3061",
+    year: 2018,
+  },
+  {
+    name: "Software Engineering Tools and Practices",
+    code: "SOEng3051",
+    year: 2018,
+  },
+  {
+    name: "Data Communication and Computer Network",
+    code: "SOEng3071",
+    year: 2018,
+  },
+  {
+    name: "Advanced Database Systems",
+    code: "SOEng3041",
+    year: 2018,
+  },
+  {
+    name: "Computer Organization and Architecture",
+    code: "SOEng3031",
+    year: 2018,
+  },
+  {
+    name: "Software Requirements Engineering",
+    code: "SOEng3081",
+    year: 2018,
+  },
+  {
+    name: "Design and Analysis of Algorithm",
+    code: "SOEng3021",
+    year: 2018,
   },
 ];
 
