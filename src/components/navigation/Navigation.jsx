546be722f1b9c4aacbed8e2fb6e02a44d5e58baa
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-24 19:57:18 +0300

    refactor: remove unused imports of icon components

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,6 +1,4 @@
-import Doc from "../../assets/file-icon/Doc.jsx";
 import AboutIcon from "../../assets/material-icon/About.jsx";
-import AboutIconColored from "../../assets/material-icon/AboutIconColored.jsx";
 import ContactIcon from "../../assets/material-icon/Contact.jsx";
 import HomeIcon from "../../assets/material-icon/Home.jsx";
 import "./Navigation.css";
