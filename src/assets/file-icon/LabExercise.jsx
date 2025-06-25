05c35c6802583074dae5c27c7277f4fbc52d42d9
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-25 07:42:07 +0300

    refactor: rename LabExercise component to LabEx and update SVG attributes

--- src/assets/file-icon/LabExercise.jsx
+++ src/assets/file-icon/LabExercise.jsx
@@ -1,58 +1,56 @@
-import React from "react";
-
-export default function LabExercise(props) {
+export default function LabEx(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width={32}
       height={32}
-      viewBox="0 0 24 24"
+      viewBox="0 0 20 20"
       {...props}
     >
       <g fill="none">
         <path
-          fill="url(#fluentColorPhoneLaptop243)"
-          d="M6.254 16L12 15.469l5.754.531h.065a1 1 0 0 1 .35.064l3.126 1.172c.423.158.704.562.705 1.014H2c0-.451.28-.855.702-1.013l3.128-1.173A1 1 0 0 1 6.181 16z"
+          fill="url(#fluentColorPhoneLaptop203)"
+          d="M15.764 13H4.236a1 1 0 0 0-.447.106l-1.342.67A.81.81 0 0 0 2 14.5a.5.5 0 0 0 .5.5h15a.5.5 0 0 0 .5-.5a.81.81 0 0 0-.447-.724l-1.342-.67a1 1 0 0 0-.447-.106"
         ></path>
         <path
-          fill="url(#fluentColorPhoneLaptop244)"
-          d="M2.75 17.5a.75.75 0 0 0 0 1.5h18.5a.75.75 0 0 0 0-1.5z"
+          fill="url(#fluentColorPhoneLaptop204)"
+          d="M2.5 14a.5.5 0 0 0 0 1h15a.5.5 0 0 0 0-1z"
         ></path>
         <path
-          fill="url(#fluentColorPhoneLaptop245)"
-          d="M4 6.25A2.25 2.25 0 0 1 6.25 4h11.5A2.25 2.25 0 0 1 20 6.25v7.5A2.25 2.25 0 0 1 17.75 16H6.25A2.25 2.25 0 0 1 4 13.75z"
+          fill="url(#fluentColorPhoneLaptop205)"
+          d="M4.5 4A1.5 1.5 0 0 0 3 5.5v6A1.5 1.5 0 0 0 4.5 13h11a1.5 1.5 0 0 0 1.5-1.5v-6A1.5 1.5 0 0 0 15.5 4z"
         ></path>
         <path
-          fill="url(#fluentColorPhoneLaptop240)"
+          fill="url(#fluentColorPhoneLaptop200)"
           fillOpacity={0.3}
-          d="M6.25 4A2.25 2.25 0 0 0 4 6.25v7.5A2.25 2.25 0 0 0 6.25 16h-.069a1 1 0 0 0-.35.064l-3.129 1.173A1.08 1.08 0 0 0 2 18.25c0 .414.336.75.75.75h18.5a.75.75 0 0 0 .75-.75v-.013a1.09 1.09 0 0 0-.705-1.001l-3.125-1.172a1 1 0 0 0-.351-.064h-.065A2.25 2.25 0 0 0 20 13.75v-7.5A2.25 2.25 0 0 0 17.75 4z"
+          d="M3 5.5A1.5 1.5 0 0 1 4.5 4h11A1.5 1.5 0 0 1 17 5.5v6a1.5 1.5 0 0 1-1.5 1.5h.264c.155 0 .308.036.447.106l1.342.67A.81.81 0 0 1 18 14.5a.5.5 0 0 1-.5.5h-15a.5.5 0 0 1-.5-.5a.81.81 0 0 1 .447-.724l1.342-.67A1 1 0 0 1 4.236 13H4.5A1.5 1.5 0 0 1 3 11.5z"
         ></path>
         <path
-          fill="url(#fluentColorPhoneLaptop241)"
-          d="M8.254 8.003c.967 0 1.75.784 1.75 1.75v9.5a1.75 1.75 0 0 1-1.75 1.75H3.75A1.75 1.75 0 0 1 2 19.253v-9.5c0-.966.784-1.75 1.75-1.75z"
+          fill="url(#fluentColorPhoneLaptop201)"
+          d="M2 8.5A1.5 1.5 0 0 1 3.5 7h4A1.5 1.5 0 0 1 9 8.5v8A1.5 1.5 0 0 1 7.5 18h-4A1.5 1.5 0 0 1 2 16.5z"
         ></path>
         <path
-          fill="url(#fluentColorPhoneLaptop242)"
-          d="M6.75 17.507A.75.75 0 0 1 6.852 19l-.102.007h-1.5a.75.75 0 0 1-.102-1.493l.102-.007z"
+          fill="url(#fluentColorPhoneLaptop202)"
+          d="M5 15a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z"
         ></path>
         <defs>
           <radialGradient
-            id="fluentColorPhoneLaptop240"
+            id="fluentColorPhoneLaptop200"
             cx={0}
             cy={0}
             r={1}
-            gradientTransform="matrix(3.5 0 0 8.14063 8.5 15.5)"
+            gradientTransform="matrix(4.5 0 0 5.59375 6 12)"
             gradientUnits="userSpaceOnUse"
           >
-            <stop offset={0.478} stopColor="#0695ff"></stop>
+            <stop offset={0.563} stopColor="#0695ff"></stop>
             <stop offset={1} stopColor="#0695ff" stopOpacity={0}></stop>
           </radialGradient>
           <radialGradient
-            id="fluentColorPhoneLaptop241"
+            id="fluentColorPhoneLaptop201"
             cx={0}
             cy={0}
             r={1}
-            gradientTransform="matrix(9.75539 18.76895 -36.34793 18.89228 -.001 3.534)"
+            gradientTransform="rotate(61.756 -2.566 1.818)scale(18.0277 35.5671)"
             gradientUnits="userSpaceOnUse"
           >
             <stop stopColor="#f08af4"></stop>
@@ -60,44 +58,44 @@ export default function LabExercise(props) {
             <stop offset={1} stopColor="#4e44db"></stop>
           </radialGradient>
           <radialGradient
-            id="fluentColorPhoneLaptop242"
+            id="fluentColorPhoneLaptop202"
             cx={0}
             cy={0}
             r={1}
-            gradientTransform="rotate(53.319 -15.519 12.818)scale(4.5513 5.2162)"
+            gradientTransform="matrix(1.8125 2.43333 -2.78883 2.0773 4.188 15.067)"
             gradientUnits="userSpaceOnUse"
           >
             <stop stopColor="#decbff"></stop>
             <stop offset={1} stopColor="#d6cfff"></stop>
           </radialGradient>
           <linearGradient
-            id="fluentColorPhoneLaptop243"
-            x1={22}
-            x2={20.765}
-            y1={18.912}
-            y2={12.254}
+            id="fluentColorPhoneLaptop203"
+            x1={18}
+            x2={17.543}
+            y1={14.857}
+            y2={11.2}
             gradientUnits="userSpaceOnUse"
           >
             <stop stopColor="#63686e"></stop>
             <stop offset={1} stopColor="#889096"></stop>
           </linearGradient>
           <linearGradient
-            id="fluentColorPhoneLaptop244"
-            x1={22}
-            x2={21.792}
-            y1={18.893}
-            y2={16.123}
+            id="fluentColorPhoneLaptop204"
+            x1={18}
+            x2={17.884}
+            y1={14.929}
+            y2={13.079}
             gradientUnits="userSpaceOnUse"
           >
             <stop stopColor="#889096"></stop>
             <stop offset={1} stopColor="#aab3bd"></stop>
           </linearGradient>
           <linearGradient
-            id="fluentColorPhoneLaptop245"
-            x1={15.2}
-            x2={15.95}
+            id="fluentColorPhoneLaptop205"
+            x1={12.8}
+            x2={13.282}
             y1={4}
-            y2={17.722}
+            y2={14.299}
             gradientUnits="userSpaceOnUse"
           >
             <stop stopColor="#54b6ff"></stop>
