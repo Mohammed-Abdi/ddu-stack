fcb45cac3fc5faf735cf999c15d6d3a9efb2f6f4
Mohammed Abdi <mohammedabdi.ta@gmail.com>
2025-06-25 15:36:52 +0300

    style: add hover effect to hamburger menu for better user interaction

--- src/components/hamburger/Hamburger.jsx
+++ src/components/hamburger/Hamburger.jsx
@@ -2,7 +2,10 @@ import "./Hamburger.css";
 
 function Hamburger({ menuIsOpen, onMenu }) {
   return (
-    <div className={`hamburger ${menuIsOpen ? "active" : ""}`} onClick={onMenu}>
+    <div
+      className={`hamburger hover-over ${menuIsOpen ? "active" : ""}`}
+      onClick={onMenu}
+    >
       <div className="bar"></div>
       <div className="bar"></div>
       <div className="bar"></div>
